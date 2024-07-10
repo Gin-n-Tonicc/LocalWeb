f430b166907e9d183d9558681262ec25b33f83cb
BelisAliosmanova <107267662+BelisAliosmanova@users.noreply.github.com>
2024-07-10 11:48:00 +0300

    create category crud

---
+++ server/src/main/java/com/localWeb/localWeb/exceptions/category/CategoryCreateException.java
@@ -0,0 +1,19 @@
+package com.localWeb.localWeb.exceptions.category;
+
+import com.localWeb.localWeb.exceptions.common.BadRequestException;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+
+/**
+ * Exception thrown when a category with the same name already exists.
+ * Sets the appropriate message using MessageSource (the messages are in src/main/resources/messages).
+ */
+public class CategoryCreateException extends BadRequestException {
+    public CategoryCreateException(MessageSource messageSource, boolean isUnique) {
+        super(
+                isUnique
+                        ? messageSource.getMessage("category.create.exists", null, LocaleContextHolder.getLocale())
+                        : messageSource.getMessage("category.create.invalid", null, LocaleContextHolder.getLocale())
+        );
+    }
+}
--- server/src/main/java/com/localWeb/localWeb/services/impl/CategoryServiceImpl.java
+++ server/src/main/java/com/localWeb/localWeb/services/impl/CategoryServiceImpl.java
@@ -1,37 +1,84 @@
 package com.localWeb.localWeb.services.impl;
 
+import com.localWeb.localWeb.exceptions.category.CategoryCreateException;
+import com.localWeb.localWeb.exceptions.category.CategoryNotFoundException;
 import com.localWeb.localWeb.models.dto.common.CategoryDTO;
+import com.localWeb.localWeb.models.entity.Category;
+import com.localWeb.localWeb.repositories.CategoryRepository;
 import com.localWeb.localWeb.services.CategoryService;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+    private final MessageSource messageSource;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, ModelMapper modelMapper, MessageSource messageSource) {
+        this.categoryRepository = categoryRepository;
+        this.modelMapper = modelMapper;
+        this.messageSource = messageSource;
+    }
 
     @Override
     public List<CategoryDTO> getAllCategories() {
-        return List.of();
+        List<Category> categories = categoryRepository.findAllByDeletedAtIsNull();
+        return categories.stream().map(category -> modelMapper.map(category, CategoryDTO.class)).toList();
     }
 
     @Override
     public CategoryDTO getCategoryById(UUID id) {
-        return null;
+        Optional<Category> category = categoryRepository.findByIdAndDeletedAtIsNull(id);
+        if (category.isPresent()) {
+            return modelMapper.map(category.get(), CategoryDTO.class);
+        }
+        throw new CategoryNotFoundException(messageSource);
     }
 
     @Override
     public CategoryDTO createCategory(CategoryDTO categoryDTO) {
-        return null;
+        try {
+            Category categoryEntity = categoryRepository.save(modelMapper.map(categoryDTO, Category.class));
+            return modelMapper.map(categoryEntity, CategoryDTO.class);
+        } catch (DataIntegrityViolationException exception) {
+            // If a category with the same name already exists
+            throw new CategoryCreateException(messageSource, true);
+        }
     }
 
     @Override
     public CategoryDTO updateCategory(UUID id, CategoryDTO categoryDTO) {
-        return null;
+        Optional<Category> existingCategoryOptional = categoryRepository.findByIdAndDeletedAtIsNull(id);
+
+        if (existingCategoryOptional.isEmpty()) {
+            throw new CategoryNotFoundException(messageSource);
+        }
+
+        Category existingCategory = existingCategoryOptional.get();
+        modelMapper.map(categoryDTO, existingCategory);
+
+        existingCategory.setId(id);
+        Category updatedCategory = categoryRepository.save(existingCategory);
+        return modelMapper.map(updatedCategory, CategoryDTO.class);
     }
 
     @Override
     public void deleteCategory(UUID id) {
-
+        Optional<Category> category = categoryRepository.findByIdAndDeletedAtIsNull(id);
+        if (category.isPresent()) {
+            // Soft delete
+            category.get().setDeletedAt(LocalDateTime.now());
+            categoryRepository.save(category.get());
+        } else {
+            throw new CategoryNotFoundException(messageSource);
+        }
     }
 }
